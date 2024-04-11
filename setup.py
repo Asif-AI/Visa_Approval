8fcc632da50174822333387537408380029690b9
asif <aasifkhan02@gmail.com>
2024-04-11 15:06:11 +0530

    Setup Created

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Asif-AI",
+    author_email="aasifkhan02@gmail.com",
+    packages=find_packages()
+)
