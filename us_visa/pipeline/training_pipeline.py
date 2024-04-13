e18fc26da43b54bce4c2140cdc697c54002bb149
asif <aasifkhan02@gmail.com>
2024-04-14 00:49:56 +0530

    Data transformation added

--- us_visa/pipeline/training_pipeline.py
+++ us_visa/pipeline/training_pipeline.py
@@ -4,22 +4,22 @@ from us_visa.logger import logging
 
 from us_visa.components.data_ingestion import DataIngestion
 from us_visa.components.data_validation import DataValidation
-#from us_visa.components.data_transformation import DataTransformation
+from us_visa.components.data_transformation import DataTransformation
 #from us_visa.components.model_trainer import ModelTrainer
 #from us_visa.components.model_evaluation import ModelEvaluation
 #from us_visa.components.model_pusher import ModelPusher
 
 from us_visa.entity.config_entity import (DataIngestionConfig,
-                                          DataValidationConfig)
-#                                          DataTransformationConfig,
+                                          DataValidationConfig,
+                                          DataTransformationConfig)
 #                                          ModelTrainerConfig,
 #                                          ModelEvaluationConfig,
 #                                          ModelPusherConfig)
                                           
 
 from us_visa.entity.artifact_entity import (DataIngestionArtifact,
-                                            DataValidationArtifact)
- #                                           DataTransformationArtifact,
+                                            DataValidationArtifact,
+                                            DataTransformationArtifact)
  #                                           ModelTrainerArtifact,
   #                                          ModelEvaluationArtifact,
   #                                          ModelPusherArtifact)
@@ -30,7 +30,7 @@ class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
-        #self.data_transformation_config = DataTransformationConfig()
+        self.data_transformation_config = DataTransformationConfig()
         #self.model_trainer_config = ModelTrainerConfig()
         #self.model_evaluation_config = ModelEvaluationConfig()
         #self.model_pusher_config = ModelPusherConfig()
@@ -79,9 +79,25 @@ class TrainPipeline:
         except Exception as e:
             raise USvisaException(e, sys) from e
 
+
+    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data transformation component
+        """
+        try:
+            data_transformation = DataTransformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                     data_transformation_config=self.data_transformation_config,
+                                                     data_validation_artifact=data_validation_artifact)
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_transformation_artifact
+        except Exception as e:
+            raise USvisaException(e, sys)
+
+
     def run_pipeline(self, )-> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
         except Exception as e:
             raise USvisaException(e, sys) from e
